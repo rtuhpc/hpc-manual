85b7477c3ff547b28a84dc86bc20ed6c0d4d7598
rtuhpc <125905225+rtuhpc@users.noreply.github.com>
2023-02-23 12:51:52 +0200

    Update conf.py

--- doc/conf.py
+++ doc/conf.py
@@ -32,7 +32,7 @@ sys.path.insert(0, os.path.abspath('../source'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 #extensions = ['sphinx.ext.autodoc']
-extensions = ['myst_parser']
+extensions = ['myst_parser','sphinx_copybutton']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
